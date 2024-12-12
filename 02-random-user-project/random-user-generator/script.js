dbc1cd719ae0110bf37e7353f76cdd4db6258745
Hlxuan <huanglangxuan@hlxuan.top>
2024-12-12 22:53:52 +0800

    feat:显示用户数据

--- 02-random-user-project/random-user-generator/script.js
+++ 02-random-user-project/random-user-generator/script.js
@@ -8,3 +8,27 @@ function featchUser() {
       hideSpinner()
     })
 }
+
+function displayUser(user) {
+  if (user.gender === "female") {
+    document.body.style.backgroundColor = "rebeccapurple"
+  } else {
+    document.body.style.backgroundColor = "steelblue"
+  }
+  // 更新DOM中的用户信息
+  const userDisplay = document.querySelector("#user")
+  userDisplay.innerHTML = `
+        <div class="flex justify-between">
+            <div class="flex">
+            <img class="w-48 h-48 rounded-full mr-8" src="${user.picture.large}" />
+                <div class="space-y-3">
+                <p class="text-xl"><span class="font-bold">姓名: </span>${user.name.first} ${user.name.last}</p>
+                <p class="text-xl"><span class="font-bold">邮箱: </span>${user.email}</p>
+                <p class="text-xl"><span class="font-bold">电话: </span>${user.phone}</p>
+                <p class="text-xl"><span class="font-bold">位置: </span>${user.location.city}, ${user.location.country}</p>
+                <p class="text-xl"><span class="font-bold">年龄: </span>${user.dob.age}</p>
+                </div>
+            </div>
+        </div>
+    `
+}
